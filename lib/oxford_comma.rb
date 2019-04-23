a72c3e79df2bf50adf4ff2e9b117cfb4f958a54e
Andrew Silva <andrewsilva9@gmail.com>
2019-04-23 01:33:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
   else
-    return array[0..-2].join(", ") + array.insert(-1, ", and ")
+    return array[0..-2].join(", ") + ", and " + array.last
   end
 end
