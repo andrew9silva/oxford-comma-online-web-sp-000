6744cac473a9547e3d8de24c6701bff6caa9d4b9
Andrew Silva <andrewsilva9@gmail.com>
2019-04-23 00:57:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
   else array.size > 2
-    return array.join(", ") + array.insert(-1 ", and ") 
+    return array.join(", ") + array.insert(", and ") 
   end
 end
